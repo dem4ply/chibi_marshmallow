5041f3877e7e008cf4884cd8d222980f3b369078
dem4ply <dem4ply@gmail.com>
2026-05-10 08:53:10 -0600

    se agrega un nuevo field para obtener los valores de context

--- chibi_marshmallow/fields.py
+++ chibi_marshmallow/fields.py
@@ -1,5 +1,7 @@
 import datetime
-from marshmallow import fields, ValidationError
+from marshmallow import fields
+from marshmallow.utils import missing as missing_
+from marshmallow.experimental.context import Context as Marsh_context
 
 
 class String_lower( fields.String ):
@@ -19,3 +21,42 @@ class Timestamp( fields.Field ):
 
     def _deserialize( self, value, attr, data, **kw ):
         return datetime.datetime.fromtimestamp( float( value ) )
+
+
+class Context( fields.Field ):
+    """
+    regresa el valor del context siendo el nombre del field la key a buscar
+    en el contexto
+    """
+
+    def __init__( self, key=None, load_default=None, **kw ):
+        super().__init__( **kw )
+        self._key = key
+
+    def deserialize( self, value, attr, data, **kw, ):
+        """
+        Deserialize el value.
+
+        Parameters
+        ----------
+        value: any
+            si es algo entonces no se usara el context
+        attr: str
+            attribute or key en el objeto a deserializar
+        data: obj
+            objeto que se mando al load
+        kw: dict
+            otros parametros que se mandaron al field
+        """
+        # Validate required fields, deserialize, then validate
+        # deserialized value
+        self._validate_missing(value)
+        if value is missing_:
+            try:
+                return Marsh_context.get()[ self._key or attr ]
+            except LookupError as e:
+                raise LookupError(
+                    "no se encontro el contexto para el serializador" ) from e
+        output = self._deserialize( value, attr, data, **kw )
+        self._validate(output)
+        return output
--- tests/test_fields.py
+++ tests/test_fields.py
@@ -3,6 +3,7 @@ import unittest
 
 from chibi_marshmallow import fields
 from marshmallow import Schema
+from marshmallow.experimental.context import Context
 
 
 class Schema_lower_test( Schema ):
@@ -11,6 +12,14 @@ class Schema_lower_test( Schema ):
 
 class Schema_timestamp_test( Schema ):
     test = fields.Timestamp()
+
+
+class Schema_context_test( Schema ):
+    test = fields.Context()
+
+
+class Schema_context_test_2( Schema ):
+    test = fields.Context( "other" )
 
 
 class Test_string_lower( unittest.TestCase ):
@@ -28,4 +37,18 @@ class Test_timestamp( unittest.TestCase ):
     def test_when_is_int_should_return_the_dateime( self ):
         expected = datetime.datetime( 1990, 1, 1 )
         result = Schema_timestamp_test().load( { 'test': 631173600 } )
+        self.assertEqual( result[ 'test' ], expected )
+
+
+class Test_context( unittest.TestCase ):
+    def test_default_key_context_should_work( self ):
+        expected = "Hello my little context!!!"
+        with Context( { "test": expected } ):
+            result = Schema_context_test().load( {} )
+        self.assertEqual( result[ 'test' ], expected )
+
+    def test_no_default_key_context_should_work( self ):
+        expected = "Hello my little context!!!"
+        with Context( { "other": expected } ):
+            result = Schema_context_test_2().load( {} )
         self.assertEqual( result[ 'test' ], expected )
